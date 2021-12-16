247f9612acf6303a9ffecb1ece1e52c86ab61ef8
quicksilverr <abbas.s@codewalla.com>
2021-12-16 19:15:18 +0530

    Scanned Barcode Snackbar

--- src/pages/auth-stack/upload-prescription-screen/index.tsx
+++ src/pages/auth-stack/upload-prescription-screen/index.tsx
@@ -1,5 +1,12 @@
-import { Box, Button, Dialog, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Dialog,
+  Snackbar,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Webcam from "react-webcam";
 import { Barcode, ScanSettings } from "scandit-sdk";
@@ -7,7 +14,8 @@ import NavBar from "../../../components/navbar";
 import ROUTES from "../../../navigation/routes";
 import ScanditBarcodeScanner from "scandit-sdk-react";
 import { KEYS } from "../../../constants/keys";
-
+import { BarcodesEntity, ScandIt } from "../../../interfaces/interface";
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
 const UploadingPrescriptionScreen: React.FC = () => {
   const navigate = useNavigate();
   const navigateToHome = () => {
@@ -15,25 +23,24 @@ const UploadingPrescriptionScreen: React.FC = () => {
   };
 
   const webcamRef = React.useRef(null);
-  const [imgSRC, setImgSRC] = useState('')
+  const [imgSRC, setImgSRC] = useState("");
   const [files, setFile] = useState<any[]>([]);
-  const [cameraDialog, setCameraDialog] = useState(false)
+  const [cameraDialog, setCameraDialog] = useState(false);
   const fileHandler = (e: any) => {
     console.log(e.target.files);
 
-    let allfiles:any[] = [];
+    let allfiles: any[] = [];
     for (let i = 0; i < e.target.files.length; i++) {
       allfiles.push(e.target.files[i]);
     }
     if (allfiles.length > 0) {
       //@ts-ignore
-      if(files.length > 0){
-          //@ts-ignore
-        setFile(allfiles).push(...files)
-      }else{
-        setFile(allfiles)
+      if (files.length > 0) {
+        //@ts-ignore
+        setFile(allfiles).push(...files);
+      } else {
+        setFile(allfiles);
       }
-      
     }
   };
 
@@ -44,24 +51,66 @@ const UploadingPrescriptionScreen: React.FC = () => {
   };
 
   const decodeBase64 = (base64data: any) => {
-    let base64ToString = Buffer.from(base64data, "base64").toString()
-    return base64ToString
-}
+    let base64ToString = Buffer.from(base64data, "base64").toString();
+    return base64ToString;
+  };
 
-const getScanSettings = () => {
-  return new ScanSettings({ enabledSymbologies: [Barcode.Symbology.CODE128, Barcode.Symbology.EAN13,Barcode.Symbology.EAN8] });
-};
+  const getScanSettings = () => {
+    return new ScanSettings({
+      enabledSymbologies: [
+        Barcode.Symbology.CODE128,
+        Barcode.Symbology.EAN13,
+        Barcode.Symbology.EAN8,
+      ],
+    });
+  };
 
-const [openDialog, setOpenDialog] = useState(false)
-
+  const [openDialog, setOpenDialog] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [scanditData, setScanditData] = useState<ScandIt>();
   const capturePhoto = React.useCallback(() => {
     //@ts-ignore
     const imageSrc = webcamRef?.current?.getScreenshot();
-    if(imageSrc){
-        setImgSRC(imageSrc)
-        setCameraDialog(false)
+    if (imageSrc) {
+      setImgSRC(imageSrc);
+      setCameraDialog(false);
     }
   }, [webcamRef]);
+
+  useEffect(() => {
+    //  console.log(scanditData)
+    if (scanditData) {
+      if (scanditData.barcodes) {
+        if (scanditData.barcodes.length > 0) {
+          console.log(scanditData.barcodes);
+          setOpenSnackbar(true);
+        }
+      }
+    }
+  }, [scanditData]);
+
+  const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref
+  ) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
+  const getSnackBar = () => {
+    {
+      scanditData.barcodes &&
+        scanditData.barcodes.length > 0 &&
+        scanditData.barcodes.map((value) => {
+          return (
+            <Snackbar
+              open={openSnackbar}
+              onClose={() => setOpenSnackbar(false)}
+              message={value}
+            />
+          );
+        });
+    }
+  };
 
   return (
     <div>
@@ -87,7 +136,7 @@ const [openDialog, setOpenDialog] = useState(false)
                 flex: 0.5,
               }}
             >
-              {(files.length > 0 || imgSRC) ? (
+              {files.length > 0 || imgSRC ? (
                 <Typography
                   sx={{
                     fontSize: "40px",
@@ -114,9 +163,8 @@ const [openDialog, setOpenDialog] = useState(false)
                   We will reach out to them, and transfer it to us.
                 </Typography>
               ) : (
-              
-              //  <img height={"500px"} width={"700px"}  src={imgSRC}  />
-                  
+                //  <img height={"500px"} width={"700px"}  src={imgSRC}  />
+
                 files.map((file, key) => {
                   return (
                     <div key={key} className="Row">
@@ -195,7 +243,7 @@ const [openDialog, setOpenDialog] = useState(false)
 
                 <Button
                   onClick={() => {
-                    setCameraDialog(true)
+                    setCameraDialog(true);
                   }}
                   sx={{
                     backgroundColor: "#0074c0",
@@ -229,7 +277,7 @@ const [openDialog, setOpenDialog] = useState(false)
 
               <Button
                 onClick={() => {
-                 setOpenDialog(true)
+                  setOpenDialog(true);
                 }}
                 sx={{
                   backgroundColor: "#0074c0",
@@ -247,61 +295,98 @@ const [openDialog, setOpenDialog] = useState(false)
           </Box>
         </Box>
       </Box>
-      <Dialog open={cameraDialog}
-      onBackdropClick={() => setCameraDialog(false)}
-      maxWidth="xl"
-      sx={{
-          alignSelf: "center"
-      }}>
+
+      <Dialog
+        open={cameraDialog}
+        onBackdropClick={() => setCameraDialog(false)}
+        maxWidth="xl"
+        sx={{
+          alignSelf: "center",
+        }}
+      >
         <>
-        <Webcam
-          audio={false}
-          height={720}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          width={1280}
-          videoConstraints={videoConstraints}
-        />
-        <Button
-          onClick={() => {
-            capturePhoto()
-          }}
+          <Webcam
+            audio={false}
+            height={720}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            width={1280}
+            videoConstraints={videoConstraints}
+          />
+          <Button
+            onClick={() => {
+              capturePhoto();
+            }}
+            sx={{
+              backgroundColor: "#0074c0",
+              color: "white",
+              borderRadius: "20px",
+              alignSelf: "center",
+              paddingRight: "100px",
+              marginTop: "-100px",
+              paddingLeft: "100px",
+            }}
+          >
+            Capture Photo
+          </Button>
+        </>
+      </Dialog>
+
+      <Dialog
+        open={openDialog}
+        onBackdropClick={() => setOpenDialog(false)}
+        sx={{}}
+      >
+        {" "}
+        <Box
           sx={{
-            backgroundColor: "#0074c0",
-            color: "white",
-            borderRadius: "20px",
-            alignSelf: "center",
-            paddingRight: "100px",
-            marginTop: "-100px",
-            paddingLeft: "100px",
+            width: "100%",
+            height: "100%",
           }}
         >
-          Capture Photo
-        </Button></>
-        
+          <ScanditBarcodeScanner
+            // Library licensing & configuration options (see https://docs.scandit.com/stable/web/globals.html#configure)
+            licenseKey={KEYS.LICENSE_KEYS}
+            engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build" // could also be a local folder, e.g. "build"
+            // Picker events
+            onReady={() => {
+              console.log("ss");
+            }}
+            onScan={(data) => {
+              setScanditData(data);
+            }}
+            onScanError={console.log}
+            // Picker options
+            playSoundOnScan={true}
+            scanSettings={getScanSettings()}
+            accessCamera={true}
+            preloadBlurryRecognition={true}
+            preloadEngine={true}
+          />
+        </Box>
       </Dialog>
-      <Dialog open={openDialog} onBackdropClick={() => setOpenDialog(false)} sx={{
-             
-            }}> <Box sx={{
-              width: '100%',
-              height: '100%'
-            }}>
-              <ScanditBarcodeScanner
-                // Library licensing & configuration options (see https://docs.scandit.com/stable/web/globals.html#configure)
-                licenseKey={KEYS.LICENSE_KEYS}
-                engineLocation="https://cdn.jsdelivr.net/npm/scandit-sdk@5.x/build" // could also be a local folder, e.g. "build"
-                // Picker events
-                onReady={() => {console.log('ss')}}
-                onScan={(data) => console.log(data)}
-                onScanError={console.log}
-                // Picker options
-                playSoundOnScan={true}
-                scanSettings={getScanSettings()}
-                accessCamera={true}
-                preloadBlurryRecognition={true}
-                preloadEngine={true}
-              />
-            </Box></Dialog>
+      {scanditData &&
+        scanditData.barcodes &&
+        scanditData.barcodes.length > 0 &&
+        scanditData.barcodes.map((value) => {
+          return (
+            <Snackbar
+              open={openSnackbar}
+              onClose={() => setOpenSnackbar(false)}
+              // message={value.data}
+              anchorOrigin={{ horizontal: "center", vertical: "bottom" }}
+              autoHideDuration={1500}
+            >
+              <Alert
+                onClose={() => setOpenSnackbar(false)}
+                severity="success"
+                sx={{ width: "100%" }}
+              >
+                {`The scanned barcode is ${value.data}`}
+              </Alert>
+            </Snackbar>
+          );
+        })}
     </div>
   );
 };
