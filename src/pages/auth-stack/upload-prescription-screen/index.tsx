2e8718596240d956159d783e2c0f0db445a000dc
DevCode <vijay1989reddy@gmail.com>
2021-12-16 19:58:06 +0530

    added interface folder

--- src/pages/auth-stack/upload-prescription-screen/index.tsx
+++ src/pages/auth-stack/upload-prescription-screen/index.tsx
@@ -117,7 +117,6 @@ const UploadingPrescriptionScreen: React.FC = () => {
     <div>
       <Box>
         <Box display="flex" flexDirection="column" height={"100vh"} flex={1}>
-          <NavBar />
           <Box
             sx={{
               display: "flex",
