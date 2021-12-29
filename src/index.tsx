2c7eab291ff328eff8ff11ac4a3a48eb599da7cd
DevCode <vijay1989reddy@gmail.com>
2021-12-29 09:55:40 +0530

    updated fonts

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,10 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../src/config/theme";
 import { CssBaseline } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
